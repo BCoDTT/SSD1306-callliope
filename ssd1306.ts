e471b7b3f5b0b415d35fe33b1c9805bbcbf56235
BCoDTT <37150614+BCoDTT@users.noreply.github.com>
2018-12-20 03:12:31 +0100

    Update ssd1306.ts

--- ssd1306.ts
+++ ssd1306.ts
@@ -272,4 +272,20 @@ namespace OLED_SSD1306 {
     export function fillTriangle(x0: number, y0: number, x1: number, y1: number, x2: number, y2: number): void {
         return;
     }
+    
+    
+    /**
+     * @param progress current progress
+     * Show a progress bar with specified percentage of progress
+     */
+    //% blockId=oled_show_progress
+    //% block="show progress of percentage progress %progress|"
+    //% async 
+    //% shim=OLED_SSD1306::showProgress
+    export function showProgress(progress:number): void {
+        return;
+    }
+
+    
+    
 }
