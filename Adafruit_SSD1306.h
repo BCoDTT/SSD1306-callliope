723c480902d899e3920d4dc1ea4a65a88952c112
BCoMak3r <37150614+BCoMak3r@users.noreply.github.com>
2018-12-21 23:41:20 +0100

    Update Adafruit_SSD1306.h

--- Adafruit_SSD1306.h
+++ Adafruit_SSD1306.h
@@ -85,7 +85,7 @@ protected:
 /** This is the I2C SSD1306 display driver transport class
  *
  */
-class Adafruit_SSD1306_I2c : public Adafruit_SSD1306
+class Adafruit_SSD1306_I2C : public Adafruit_SSD1306
 {
 public:
 	#define SSD_I2C_ADDRESS     0x78
@@ -101,7 +101,7 @@ public:
 	 * @param rawHeight - The vertical number of pixels for the display, defaults to 32
 	 * @param rawWidth - The horizonal number of pixels for the display, defaults to 128
 	 */
-	Adafruit_SSD1306_I2c(MicroBitI2C i2c, uint8_t i2cAddress = SSD_I2C_ADDRESS, uint8_t rawHeight = 32, uint8_t rawWidth = 128)
+	Adafruit_SSD1306_I2C(MicroBitI2C i2c, uint8_t i2cAddress = SSD_I2C_ADDRESS, uint8_t rawHeight = 32, uint8_t rawWidth = 128)
 	    : Adafruit_SSD1306(rawHeight, rawWidth)
 	    , mi2c(i2c)
 	    , mi2cAddress(i2cAddress)
