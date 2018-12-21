cc35fa1bb57ec40e49f3f84cab0b855448a8bcb7
BCoMak3r <37150614+BCoMak3r@users.noreply.github.com>
2018-12-21 23:38:44 +0100

    Update and rename oled-ssd1306.ts to oled_ssd1306.ts

--- oled_ssd1306.ts
+++ oled_ssd1306.ts
@@ -4,7 +4,7 @@
  
 //% weight=91 color=#00A654 icon="\uf1b9" block="OLED:SSD1306"
 
-namespace oled-ssd1306 {
+namespace oled_ssd1306 {
 
   /***********************************************************************************************
    * micro:bit/calliope OLED:SSD1306 / :PXT blocks
@@ -18,7 +18,7 @@ namespace oled-ssd1306 {
      * ToDo: @param address adddress (in HEX)       # Module's I2C address
      */
     //% blockId=oled_init_terminal block="initialize OLED with height %height|width %width"
-    //% shim=oled-ssd1306::init_terminal
+    //% shim=oled_ssd1306::init_terminal
     export function init(height: number = 64, width: number = 128): void {
         return;
     }
@@ -28,7 +28,7 @@ namespace oled-ssd1306 {
      * Clear OLED display
      */
     //% blockId=oled_clear_screen block="clear OLED display"
-    //% shim=oled-ssd1306::clearDisplay
+    //% shim=oled_ssd1306::clearDisplay
     export function clear(): void {
         return;
     }
@@ -40,7 +40,7 @@ namespace oled-ssd1306 {
      * @param y         - absolute y-position (in pixels)
      */
     //% blockId=oled_settextcursor_xy block="set text cursor to position x %x|y %y" 
-    //% shim=oled-ssd1306::setTextCursorXY
+    //% shim=oled_ssd1306::setTextCursorXY
     export function setTextCursorXY(x: number, y: number): void {
         return;
     }
@@ -52,7 +52,7 @@ namespace oled-ssd1306 {
      * @param column    - column
      */
     //% blockId=oled_settextcursor_row_col block="set text cursor to row %row|column %column" 
-    //% shim=oled-ssd1306::setTextCursorRowColumn
+    //% shim=oled_ssd1306::setTextCursorRowColumn
     export function setTextCursorRowColumn(row: number, column: number): void {
         return;
     }
@@ -63,7 +63,7 @@ namespace oled-ssd1306 {
      * @param text - text to display on OLED display
      */
     //% blockId=oled_print_string block="show|string %text" 
-    //% shim=oled-ssd1306::showString
+    //% shim=oled_ssd1306::showString
     export function showString(text: string): void {
         console.log("display: " + text);
         return;
@@ -75,7 +75,7 @@ namespace oled-ssd1306 {
      * @param number - number to display on OLED display
      */
     //% block="show|number %number" blockId=oled_print_number
-    //% shim=oled-ssd1306::showNumber
+    //% shim=oled_ssd1306::showNumber
     export function showNumber(number: number): void {
         console.log("display: " + number);
         return;
@@ -87,7 +87,7 @@ namespace oled-ssd1306 {
      * @param on on     - switch display on or off
      */
     //% blockId=oled_onOffDisplay block="switch OLED display|on %on" 
-    //% shim=oled-ssd1306::onOffDisplay
+    //% shim=oled_ssd1306::onOffDisplay
     export function onOffDisplay(on: boolean = true): void {
         console.log("display: " + on );
         return;
@@ -101,7 +101,7 @@ namespace oled-ssd1306 {
      * @param r     - radius of circle (in pixels)
      */
     //% blockId=oled_drawCircle block="draw circle with x %x|y %y|r %r"
-    //% shim=oled-ssd1306::drawCircle
+    //% shim=oled_ssd1306::drawCircle
     export function drawCircle(x: number, y: number, r: number): void {
         return;
     }
@@ -114,7 +114,7 @@ namespace oled-ssd1306 {
      * @param r     - radius of circle (in pixels)
      */
     //% blockId=oled_fillCircle block="fill circle with x %x|y %y|r %r"
-    //% shim=oled-ssd1306::fillCircle
+    //% shim=oled_ssd1306::fillCircle
     export function fillCircle(x: number, y: number, r: number): void {
         return;
     }
@@ -128,7 +128,7 @@ namespace oled-ssd1306 {
      * @param y1    - y-position of end coordinate (in pixels)
      */
     //% blockId=oled_drawLine block="draw line with x0 %x0|y0 %y0|x1 %x1|y1 %y1" 
-    //% shim=oled-ssd1306::drawLine
+    //% shim=oled_ssd1306::drawLine
     export function drawLine(x0: number, y0: number, x1: number, y1: number): void {
         return;
     }
@@ -142,7 +142,7 @@ namespace oled-ssd1306 {
      * @param h     - height of rectangle (in pixels)
      */
     //% blockId=oled_drawRect block="draw rect with x %x|y %y|w %w|h %h"
-    //% shim=oled-ssd1306::drawRect
+    //% shim=oled_ssd1306::drawRect
     export function drawRect(x: number, y: number, w: number, h: number): void {
         return;
     }
@@ -156,7 +156,7 @@ namespace oled-ssd1306 {
      * @param h     - height of rectangle (in pixels)
      */
     //% blockId=oled_fillRect block="fill rect with x %x|y %y|w %w|h %h"
-    //% shim=oled-ssd1306::fillRect
+    //% shim=oled_ssd1306::fillRect
     export function fillRect(x: number, y: number, w: number, h: number): void {
         return;
     }
@@ -171,7 +171,7 @@ namespace oled-ssd1306 {
      * @param progress  - progress (in percentage)
      */
     //% blockId=oled_show_progress block="show progress bar with x %x|y %y|width %width|height %height|progress %progress"
-    //% shim=oled-ssd1306::showProgress
+    //% shim=oled_ssd1306::showProgress
     export function showProgress(x: number, y: number, width: number, height: number, progress: number): void {
         return;
     }
