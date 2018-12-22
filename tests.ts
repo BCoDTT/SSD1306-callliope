c296bce9eef71ad3a792fbf9b228a13e14cdc3c6
BCoMak3r <37150614+BCoMak3r@users.noreply.github.com>
2018-12-22 03:45:29 +0100

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -1,11 +1,11 @@
 // tests go here; this will not be compiled when this package is used as a library
 
-OLED_SSD1306.init(64, 128)
-OLED_SSD1306.showString("Hello world from calliope!")
-OLED_SSD1306.showString("counter:")
+oles_ssd1306.init(64, 128)
+oled_ssd1306.showString("Hello world from calliope!")
+oled_ssd1306.showString("counter:")
 let item = 0
 basic.forever(() => {
     basic.pause(1000)
     item += 1
-    OLED_SSD1306.showNumber(item)
+    oled_ssd1306.showNumber(item)
 })
