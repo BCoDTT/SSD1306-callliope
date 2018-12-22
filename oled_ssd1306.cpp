26ffcf3c96f5fc8348f9fca711b9fb6dbbc397ed
BCoMak3r <37150614+BCoMak3r@users.noreply.github.com>
2018-12-22 01:50:30 +0100

    Update oled_ssd1306.cpp

--- oled_ssd1306.cpp
+++ oled_ssd1306.cpp
@@ -155,7 +155,7 @@ namespace oled_ssd1306 {
 
 	
 	//%
-	void showProgress(int x, int y, int width, int height, int progress) {
+	void showProgressBar(int x, int y, int width, int height, int progress) {
 		int widthPb;
 		widthPb = progress * width/100;		
 		::drawRect(x,y,width,height);
