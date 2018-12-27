6ab7e5d845c2981076016b577918567eb2db7e34
BCoMak3r <20161718@gmx.de>
2018-12-27 17:54:41 +0100

    Vor Umbau

--- oled_ssd1306.ts
+++ oled_ssd1306.ts
@@ -261,7 +261,7 @@ namespace oled_ssd1306 {
      * Indicates changes
      * @param percentage   - degree of done
      */
-    //% blockId=oled_version_id block="version id 20181227-1740"
+    //% blockId=oled_version_id block="version id 20181227-1755"
     //% blockGap=6
     export function versionId(): void {
         return;
