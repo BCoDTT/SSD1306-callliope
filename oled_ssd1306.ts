0d8f8f208ea7400201b11b4ae007e66e0a951b1d
BCoMak3r <20161718@gmx.de>
2018-12-29 16:21:47 +0100

    drawLine2 mit direction of rotation Fix-6

--- oled_ssd1306.ts
+++ oled_ssd1306.ts
@@ -274,7 +274,7 @@ namespace oled_ssd1306 {
      /**
      * Indicates versionId
      */
-    //% blockId=oled_version_id block="version id 20181229-1604"
+    //% blockId=oled_version_id block="version id 20181229-1620"
     //% blockGap=6
     export function versionId(): void {
         return;
@@ -283,4 +283,7 @@ namespace oled_ssd1306 {
 	// #
 	
 }
+
+
+
 
--- oled_ssd1306_enums.d.ts
+++ oled_ssd1306_enums.d.ts
@@ -14,3 +14,5 @@ declare namespace oled_ssd1306 {
 	}
 }
 
+
+
