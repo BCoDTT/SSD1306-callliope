fac9f1062130c2aedb78934e04df494ba0c29bf7
BCoMak3r <20161718@gmx.de>
2018-12-29 15:58:22 +0100

    drawLine2 mit direction of rotation Fix-4

--- oled_ssd1306_enums.d.ts
+++ oled_ssd1306_enums.d.ts
@@ -1,14 +1,16 @@
+
 // Enums for pxt-calliope-oled-ssd1306
+
 declare namespace oled_ssd1306 {
 
 	enum Colors {
-		Black = 0,
-		White = 1
+		"Black" = 0,
+		"White" = 1
 	}
 
 	enum DirectionsOfRotation {
-		clockwise = true,
-		anti-clockwise = false
+		"clockwise" = true,
+		"anti-clockwise" = false
 	}
 }
 
