38d6165c68d29f416bfea720f933c8107e500b60
BCoMak3r <20161718@gmx.de>
2019-01-02 00:45:55 +0100

    Introduced drawBitmap - Fix-2

--- oled_ssd1306.ts
+++ oled_ssd1306.ts
@@ -310,7 +310,7 @@ namespace oled_ssd1306 {
      /**
      * Indicates versionId
      */
-    //% blockId=oled_version_id block="version id 20190102-0016"
+    //% blockId=oled_version_id block="version id 20190102-0045"
     //% blockGap=6
     export function versionId(): void {
         return;
